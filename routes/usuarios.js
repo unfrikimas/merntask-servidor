251828b49f41fb3ad978dfb7e7467574fc000ebb
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-10 20:39:11 +0100

    Agregando validacion a la API

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const {check} = require('express-validator');
 
 // Crea un usuario
 // api/usuarios
-router.post('/', 
+router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'El password debe tener al menos 6 caracteres').isLength({min: 6})
+    ],
     usuarioController.crearUsuario
 );
 module.exports = router;
