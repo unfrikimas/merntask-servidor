d7ce2f76dbca1f11e6202f4c93ff439eec144cfd
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-11 20:45:50 +0100

    Verificando si el usuario esta autenticado para crear nuevos  proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -6,6 +6,11 @@ exports.crearProyecto = async (req, res) => {
 
         //crear un nuevvo proyecto
         const proyecto = new Proyecto(req.body);
+        
+        //guardar el creador via JWT
+        proyecto.creador = req.usuario.id
+
+        //guardamos el proyecto
         proyecto.save();
         res.json(proyecto);
         
---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+
+    //leer el token del header
+    const token = req.header('x-auth-token');
+    console.log(token);
+
+    // revisar si no hay token
+    if(!token) {
+        return res.status(401).json({msg: 'No hay Token, permiso no valido'});
+    }
+
+    //validar el token
+    try {
+        const cifrado = jwt.verify(token, process.env.SECRETA);
+        req.usuario = cifrado.usuario;
+        next();
+    } catch (error) {
+        res.status(401).json({msg: 'Token no válido'});
+    }
+
+}
