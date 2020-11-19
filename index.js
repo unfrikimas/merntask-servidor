f23edc051d1afb9afc3c4f4ba507b0a197428fe8
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-11-19 21:12:21 +0100

    puerto

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ conectarDB();
 app.use(express.json({ extended: true }));
 
 //puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -33,6 +33,8 @@ app.use('/api/tareas', require('./routes/tareas'));
 // });
 
 //arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor activo en puerto ${port}`);
-})
+app.listen(port)
+console.log(`Servidor activo en puerto ${port}`);
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`Servidor activo en puerto ${port}`);
+// })
